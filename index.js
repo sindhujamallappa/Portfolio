28d88eb379fd0b6b2a89a926ab3828ed4abaf4ab
sindhujamallappa <sindhujamallappa@gmail.com>
2021-07-18 20:14:10 +0530

    master

---
+++ index.js
@@ -0,0 +1,36 @@
+var names = ['Graphic Designer', 'Web Designer', 'Freelancer', 'Content Editor'];
+
+setInterval(function() {
+  var rand = Math.floor(Math.random() * 4);
+  document.getElementById("name").innerHTML = names[rand];
+}, 2000);
+
+  function Downloadfile(Passion) {
+    //Set the File URL.
+    var url = "Passion.pdf" + Passion;
+
+    //Create XMLHTTP Request.
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "blob";
+    req.onload = function () {
+        //Convert the Byte Data to BLOB object.
+        var blob = new Blob([req.response], { type: "application/octetstream" });
+
+        //Check the Browser type and download the File.
+        var isIE = false || !!document.documentMode;
+        if (isIE) {
+            window.navigator.msSaveBlob(blob, fileName);
+        } else {
+            var url = window.URL || window.webkitURL;
+            link = url.createObjectURL(blob);
+            var a = document.createElement("a");
+            a.setAttribute("download", fileName);
+            a.setAttribute("href", link);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    };
+    req.send();
+};
